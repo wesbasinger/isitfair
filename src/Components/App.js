64bd766a174a2f1522b0bf1c0e46eb37ae65b1a9
Wes Basinger <wbasinger@villagetechschools.org>
2018-02-18 14:28:44 +0000

    Worked on the card game, increase bet and decrease bet.

--- src/Components/App.js
+++ src/Components/App.js
@@ -32,7 +32,7 @@ class App extends React.Component {
                 <Header cash={this.state.cash} />
                 <Switch>
                     <Route exact path="/" component={Landing} />
-                    <Route path="/cards" component={Cards} />
+                    <Route path="/cards" render={()=><Cards cash={this.state.cash}/>} />
                     <Route path="/dice"  render={()=><Dice applyWinnings={this.applyWinnings}/>}/>
                     <Route path="/spinner" component={Spinner} />
                 </Switch>
--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,9 +1,56 @@
 import React from 'react';
+import { newDeck } from '52-deck';
 
-const Cards = () => {
-    return(
-        <div>This is a CardGame.</div>
-    )
+const deck = newDeck();
+
+
+class Cards extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            bet: 0
+        }
+    }
+    
+    render() {
+        return(
+            <div>
+                <h1>Card Game</h1>
+                <h2>Instructions</h2>
+                <p>
+                    This game allows you place a wager.  You will place your wager
+                    first and then draw a card to see what your payout will be.
+                    Face cards pay out double the bet, everything else loses.
+                </p>
+                <p>
+                    BET: ${this.state.bet}
+                </p>
+                <button onClick={
+                    (e)=> {
+                        if(this.props.cash <= this.state.bet) {
+                            alert("You are out of cash and cannot increase your bet.");
+                        } else {
+                            const incrementBet = this.state.bet + 1
+                            this.setState({bet: incrementBet})   
+                        }
+                    }
+                }>Increase Bet</button>
+                <button onClick={
+                    (e)=> {
+                        if(this.state.bet===0) {
+                            alert("You cannot bet a negative amount.");
+                        } else {
+                            const decrementBet = this.state.bet - 1;
+                            this.setState({bet: decrementBet});
+                        }
+                    }
+                }>Decrease Bet</button>
+                <br />
+                <button>Draw Card</button>
+            </div>
+        )
+    }
 }
 
 export default Cards;
